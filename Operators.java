c6fa56d79316a050c76c1323b653e2a172d56f08
Dusko_Sofronievski <dukisofronievski@gmail.com>
2024-10-06 20:35:43 +0200

    Commit

---
+++ Operators.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Operators { public static void main(String[] args) {
+
+    int num1 = 4;
+    int num2 = 2;
+
+    int sum = num1 + num2;
+    System.out.println("The summery is : \s");
+    System.out.println(sum);
+    int sub = num1 - num2;
+    System.out.println("The subtraction is : \s");
+    System.out.println (sub);
+    double mul = num1 * num2;
+    System.out.println("The multiplication is : \s");
+    System.out.println(mul);
+    float div = (num1/num2);
+    System.out.println("The division is : \s");
+    System.out.println(div);
+
+
+}
+}
